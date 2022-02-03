3281baa5f1e39b6ce82ba079aac2b6212cac562f
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2022-02-03 18:20:19 +0800

    more changes to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,25 @@
 import { Button, TextField, Typography } from '@mui/material';
 import React from 'react';
 import NavBar from '../components/NavBar';
+import loginArtwork from '../assets/loginArtwork.png';
 
 function Login() {
     return (
-        <div style={{backgroundColor: 'red', width: '100%', height: window.innerHeight, display: 'flex', flexDirection: 'column'}}>
+        <div style={{width: '100%', height: window.innerHeight, display: 'flex', flexDirection: 'column'}}>
             {/* NavBar Area */}
-            <div style={{height: '6em', backgroundColor: 'cyan', display: 'flex', flexDirection: 'column'}}>
+            <div style={{backgroundColor: 'cyan', display: 'flex', flexDirection: 'column'}}>
                 <NavBar/>
             </div>
 
             {/* Login Area */}
             <div style={{display: 'flex', flexGrow: 1, flexDirection: 'row'}}>
                 {/* Background Element */}
-                <div style={{flexGrow: 1, display: 'flex', backgroundColor: 'green'}}>
-                    Put artwork here
+                <div style={{flexGrow: 1, display: 'flex'}}>
+                    <img src={loginArtwork} style={{width: '40em', margin: 'auto'}}/>
                 </div>
 
                 {/* Login Text and Form */}
-                <div style={{width: '30em', backgroundColor: 'orange', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+                <div style={{width: '30em', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
                     <div style={{flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '0em 3em 0em 5em'}}>
                         <Typography variant='h4'>Selamat Datang di</Typography>
                         <Typography variant='h4'>Sistem Informasi Manajemen Kinerja Guru</Typography>
@@ -27,7 +28,7 @@ function Login() {
                             <TextField variant='outlined' label='Nama Pengguna' style={{margin: '2em 0em 0em 0em'}}/>
                             <TextField variant='outlined' label='Kata sandi' style={{margin: '1em 0em 0em 0em'}}/>
                             <Typography style={{margin: '0em 0em 1em 0em', alignSelf: 'end'}}>Lupa kata sandi?</Typography>
-                            <Button variant='contained' color='primary'>Masuk</Button>
+                            <Button variant='contained' color='primary' style={{height: '4em', width: '10em', alignSelf: 'end'}}>Masuk</Button>
                         </div>
                     </div>
                 </div>
