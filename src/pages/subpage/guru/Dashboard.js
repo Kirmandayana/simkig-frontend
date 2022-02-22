58095a7d49be1fdf859e6f789ac65b57d6dd0122
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2022-02-22 20:37:01 +0800

    fix dashboard layout

--- src/pages/subpage/guru/Dashboard.js
+++ src/pages/subpage/guru/Dashboard.js
@@ -5,16 +5,16 @@ import homeArtWork from '../../../assets/homeArtWork.png';
 function Dashboard() {
   return (
     <div style={{display:'flex', flexDirection: 'row', flexGrow:1}}>
-      <div style={{paddingLeft: '3em', marginTop: 'auto', marginBottom: 'auto'}}>
-        <Typography variant='h3'>Selamat Datang di Sistem Informasi Manajemen Kinerja Guru</Typography>
-        <Typography variant='h5' style={{paddingTop: '1em'}}>
+      <div style={{paddingLeft: '9em', marginTop: 'auto', marginBottom: 'auto'}}>
+        <Typography style={{fontSize: '1.5em', fontWeight: 'bold'}}>Selamat Datang di Sistem Informasi Manajemen Kinerja Guru</Typography>
+        <Typography style={{paddingTop: '1em'}}>
           Pada Sistem ini, anda dapat mengunggah hasil dokumentasi kegiatan belajar mengajar dikelas, 
           dan juga dapat melihat daftar laporan yang sudah dikirim maupun yang belum dikirim.
         </Typography>
       </div>
 
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft: '10em'}}>
-        <img src={homeArtWork} style={{width: '20em', margin: 'auto', paddingRight:'4em'}}/>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft: '5em'}}>
+        <img src={homeArtWork} style={{width: '15em', paddingRight:'8em'}} alt=''/>
       </div>
     </div>
   )
