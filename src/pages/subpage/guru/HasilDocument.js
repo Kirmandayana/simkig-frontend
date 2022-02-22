99403a01ddd847afc7495be50942a514a0076e43
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2022-02-22 23:48:27 +0800

    implemented getting documents from backend

--- src/pages/subpage/guru/HasilDocument.js
+++ src/pages/subpage/guru/HasilDocument.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useState} from 'react'
 import { 
   Button, 
@@ -17,14 +17,6 @@ import {
 } from '@mui/material';
 const {BACKEND_URL} = require('../../../globals')
 
-/**
- * @name getMonthRange
- * @description get the first and last day of the month and returns two strings
- * @param {number} month - month number
- * @param {number} year - year number
- * @returns {string[]} - array of two date strings
- * @example getMonthRange(1, 2020) // ['2020-01-01', '2020-01-31']
- */
 const getMonthRange = (month, year) => [
   `${year}-${month.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})}-01`,
   `${year}-${month.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})}-${new Date(year, month, 0).getDate()}`
@@ -81,9 +73,7 @@ const FilterBar = ({setData}) => {
         style={{height: '100%'}}
         onClick={() => {
           //make sure month and year is valid
-          if(month && year) {
-            console.log(month, year)
-          } else {
+          if(!month || !year) {
             alert('Mohon isi bulan dan tahun dengan benar')
             return
           }
@@ -109,8 +99,80 @@ const FilterBar = ({setData}) => {
   )
 }
 
+const DocumentRow = ({row, index}) => {
+  const [img, setImg] = useState('')
+  
+  useEffect(() => {
+    if(row.photoFilename)
+      fetch(BACKEND_URL + `/uploads/${row.photoFilename}`, {
+        headers: {
+          'access-token': localStorage.getItem('accessToken')
+        }
+      })
+      .then(res => res.blob())
+      .then(blob => {
+        const url = URL.createObjectURL(blob)
+        setImg(url)
+      })
+  }, [])
+
+  let backgroundColor
+
+  //zebra coloring on table
+  if(index % 2 === 0)
+    if(!row.photoFilename) backgroundColor = '#ffcccc'
+    else backgroundColor = '#e0e0e0'
+  else
+    if(!row.photoFilename) backgroundColor = '#ffb8b8'
+    else backgroundColor = '#ffffff'
+
+  return (
+    <TableRow style={{backgroundColor}}>
+      <TableCell component="th" scope="row" align="left">
+        {row.date}
+      </TableCell>
+      {
+        row.photoFilename ?
+        <>
+          <TableCell align="left">{row.className}</TableCell>
+          <TableCell align="left">{row.mataPelajaran}</TableCell>
+          <TableCell align="center">{row.jumlahSiswaAktif}</TableCell>
+          <TableCell align="center">
+            <img src={img} style={{height: '4em'}} />
+          </TableCell>
+          <TableCell align="left">{row.keluhan}</TableCell>
+        </>
+        :
+        <>
+          <TableCell align="center" colSpan={5}>BELUM DI ISI</TableCell>
+        </>
+      }
+    </TableRow>
+  )
+}
+
 function HasilDocument() {
   const [data, setData] = useState([])
+
+  useState(() => {
+    //fetch the data for current month upon mounting
+    fetch(BACKEND_URL + `/api/document/getDocumentList`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': localStorage.getItem('accessToken')
+      },
+      body: JSON.stringify({
+        rangeStart: getMonthRange(new Date().getMonth() + 1, new Date().getFullYear())[0],
+        rangeEnd: getMonthRange(new Date().getMonth() + 1, new Date().getFullYear())[1]
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      setData(res)
+    })
+    
+  }, [])
 
   return (
     <div style={{display:'flex', flexDirection: 'column', flexGrow:1, alignItems: 'center', marginTop: '1em'}}>
@@ -128,18 +190,9 @@ function HasilDocument() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {data.map((row, index) => (
-                  <TableRow key={index} style={{backgroundColor: index % 2 === 0 ? '#e0e0e0' : '#ffffff'}}>
-                    <TableCell component="th" scope="row" align="left">
-                      {row.date}
-                    </TableCell>
-                    <TableCell align="left">{row.className}</TableCell>
-                    <TableCell align="left">{row.mataPelajaran}</TableCell>
-                    <TableCell align="center">{row.jumlahSiswaAktif}</TableCell>
-                    <TableCell align="center">{row.photoFilename}</TableCell>
-                    <TableCell align="left">{row.keluhan}</TableCell>
-                  </TableRow>
-                ))}
+              {data.map((row, index) => 
+                  <DocumentRow row={row} key={row.id} index={index}/>
+              )}
             </TableBody>
           </Table>
       </TableContainer>
