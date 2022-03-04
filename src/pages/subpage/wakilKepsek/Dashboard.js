269d0ecfdd943ae31bd39cb8b8faa719afffba27
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2022-03-05 00:57:31 +0800

    implemented getting real data from backend on dashboard

--- src/pages/subpage/wakilKepsek/Dashboard.js
+++ src/pages/subpage/wakilKepsek/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Paper, Typography} from '@mui/material'
 import {
   Chart as ChartJS, 
@@ -11,6 +11,10 @@ import {
   Legend } from 'chart.js'
 import { Pie, Line } from 'react-chartjs-2'
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { BACKEND_URL } from '../../../globals';
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+dayjs.extend(utc)
 
 ChartJS.register(
   ArcElement,
@@ -20,63 +24,111 @@ ChartJS.register(
   LineElement,
   Title, Tooltip, Legend);
 
-// Pie Char Data Sementara
-export const dataPieChart = {
-  labels: ['Tidak Hadir', 'Hadir'],
-  datasets: [
-    {
-      data: [12, 19],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+const StudentAttendanceSumsChart = ({attendances}) => {
+  attendances = attendances ? attendances : {data: [], dates: []}
 
-//Line Chart
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-const dataLineChart = {
-  labels: labels,
-  datasets: [{
-    label: 'Kehadiran Siswa',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  }]
-};
+  //Line Chart
+  const dataLineChart = {
+    labels: attendances.dates,
+    datasets: [{
+      label: 'Total Kehadiran Siswa',
+      data: attendances.data.map(el => el.count),
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }]
+  }
 
-//Data Table Sementara
-function createData(NIK, namaGuru, keterangan) {
-  return { NIK, namaGuru, keterangan };
+  return (
+    <Line data={dataLineChart} />
+  )
 }
 
-const rows = [
-  createData(1234567890, 'Guru A', 'Sakit'),
-  createData(1234567890, 'Guru B', 'Izin'),
-  createData(1234567890, 'Guru C', 'Sakit'),
-  createData(1234567890, 'Guru D', 'Izin'),
-  createData(1234567890, 'Guru E', 'Sakit'),
-];
+const AbsencePieChart = ({absences}) => {
+  absences = absences ? absences : {absentList: [], teachersCount: 0}
+
+  const dataPieChart = {
+    labels: ['Tidak Hadir', 'Hadir'],
+    datasets: [
+      {
+        data: [absences.absentList.length, absences.teachersCount - absences.absentList.length],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+
+  return (
+    <Pie data={dataPieChart} style={{paddingBottom:'1em'}} />
+  )
+}
+
+const AbsenceRow = ({row, index}) => {
+  return (
+    <TableRow key={index}>
+      <TableCell component="th" scope="row" align="center">
+        {row.username}
+      </TableCell>
+      <TableCell align="center">{row.fullName}</TableCell>
+      <TableCell align="center">{row.alasan}</TableCell>
+    </TableRow>
+  )
+}
 
 function Dashboard() {
+  const [absences, setAbsences] = React.useState(null)
+  const [studentAttendance, setStudentAttendance] = React.useState(null)
+
+  useEffect(() => {
+    fetch(BACKEND_URL + '/api/aggregate/getAbsentTeachers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': localStorage.getItem('accessToken')
+      },
+      body: JSON.stringify({
+        date: Date.now()
+      })
+    }).then(resp => {
+      resp.status === 200 ? 
+      resp.json().then(data => setAbsences(data)) : 
+      resp.json().then(data => console.log(data))
+    }).catch(err => console.log(err))
+
+    fetch(BACKEND_URL + '/api/aggregate/getAttendanceCount', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': localStorage.getItem('accessToken')
+      },
+      body: JSON.stringify({
+        date: Date.now()
+      })
+    }).then(resp => {
+      resp.status === 200 ? 
+      resp.json().then(data => setStudentAttendance(data)) : 
+      resp.text().then(data => console.log(data))
+    }).catch(err => console.log(err))
+  }, [])
+
   return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
     
       <div style={{display: 'flex'}}>
-        <Paper>
-          <Typography style={{paddingLeft: '1em'}}>Kehadiran Guru</Typography>
-          <Pie data={dataPieChart} style={{paddingBottom:'1em'}} />
+        <Paper style={{display: 'flex', flexDirection: 'column'}}>
+          <Typography style={{paddingLeft: '1em', alignSelf: 'center'}}>Kehadiran Guru hari ini</Typography>
+          <AbsencePieChart absences={absences}/>
         </Paper>
 
-        <Paper style={{flexGrow: 1}}>
-          <Line data={dataLineChart} />
+        <Paper style={{flexGrow: 1, marginLeft: '1em'}}>
+          <StudentAttendanceSumsChart attendances={studentAttendance}/>
         </Paper>
       </div>
 
@@ -91,15 +143,9 @@ function Dashboard() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row, index) => (
-                <TableRow key={index}>
-                  <TableCell component="th" scope="row" align="left">
-                    {row.NIK}
-                  </TableCell>
-                  <TableCell align="left">{row.namaGuru}</TableCell>
-                  <TableCell align="center">{row.keterangan}</TableCell>
-                </TableRow>
-              ))}
+              {absences ? absences.absentList.map((row, index) => (
+                <AbsenceRow key={index} row={row} />
+              )) : <TableRow></TableRow>}
             </TableBody>
           </Table>
         </TableContainer>
